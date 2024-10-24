922559fe0e87292bead8d0956a763b86943edc48
quicklike <quicklikerfree@gmail.com>
2024-10-25 00:07:28 +0300

    check user_member_status

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def message_handle(message: types.Message):
     chat_id = message.chat.id
     logging.info(f'Сообщение от пользователя с ID {user_id} из чата с ID {chat_id}. {message.text}')
     if chat_id == user_id:
-        user_channel_status = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
+        user_channel_status = await bot.get_chat_member(chat_id=ANONIM_GROUP_ID, user_id=user_id)
         if user_channel_status['status'] == 'member':
             username = message.from_user.username
             full_name = (f'ID ({user_id}) {message.from_user.first_name} {message.from_user.last_name}'.strip() +
